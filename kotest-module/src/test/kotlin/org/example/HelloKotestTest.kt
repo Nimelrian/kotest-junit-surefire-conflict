296bbe236a45bcd7f5925e4ce97c82e42376cac3
Sebastian Kappen <sebastian@kappen.dev>
2021-11-06 01:54:04 +0100

    first commit

---
+++ kotest-module/src/test/kotlin/org/example/HelloKotestTest.kt
@@ -0,0 +1,13 @@
+package org.example
+
+import io.kotest.core.spec.style.DescribeSpec
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+internal class PointParserTest : DescribeSpec({
+    describe("describe") {
+        it("should work") {
+
+        }
+    }
+})
